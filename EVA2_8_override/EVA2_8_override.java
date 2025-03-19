638797394a7caa297d764e185852603714b129b1
Diego Alejandro Salas Lopez de Lara <159965511+Diegolin220@users.noreply.github.com>
2025-03-19 00:34:02 -0600

    EVA2_8_OVERRIDE

---
+++ EVA2_8_override/EVA2_8_override.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_8_override;
+
+/**
+ *
+ * @author diegi
+ */
+public class EVA2_8_override {
+
+    public static void main(String[] args) {
+    Grabación grav = new Grabación("Los panchos", "Piel Canela", 1980, "Luis Alfonso", "Chihuahua");
+        System.out.println(grav);
+    Libro lib = new Libro("Diego", "Los tres mosqueteros", 1980, "Los buenos libros", "Chihuahua");
+        System.out.println(lib);
+    }
+}
---
+++ EVA2_8_override/Ficha_APA.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.eva2_8_override;
+
+/**
+ *
+ * @author diegi
+ */
+public class Ficha_APA {
+    private String autor;
+    private String titulo;
+    private int year;
+    private String ciudad;
+
+    public Ficha_APA() {
+        this.autor = "Ficha Apa";
+        this.titulo = "Ficha Apa";
+        this.year = 0;
+        this.ciudad="Ciudad";
+                
+    }
+
+    public Ficha_APA(String autor, String titulo, int year, String ciudad) {
+        this.autor = autor;
+        this.titulo = titulo;
+        this.year = year;
+        this.ciudad = ciudad;
+    }
+   
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    @Override
+    public String toString(){
+        String cade = "Autor: " + autor + "\n" +
+        "Titulo: " + titulo + "\n" +
+         "Year: " + year + "\n" +
+                "Ciudad: " + ciudad;
+        return cade;
+    }
+
+}
