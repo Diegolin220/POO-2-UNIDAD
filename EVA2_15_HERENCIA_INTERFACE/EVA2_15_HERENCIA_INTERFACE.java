10c2fe15d1739f2fec273ccab7bcc74971faf71f
Diego Alejandro Salas Lopez de Lara <159965511+Diegolin220@users.noreply.github.com>
2025-04-03 23:35:20 -0600

    EVA2_15_HERENCIA_INTERFACE

---
+++ EVA2_15_HERENCIA_INTERFACE/EVA2_15_HERENCIA_INTERFACE.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_15_herencia_interface;
+
+/**
+ *
+ * @author diegi
+ */
+public class EVA2_15_HERENCIA_INTERFACE {
+
+    public static void main(String[] args) {
+    
+    }
+}
+interface Prueba{
+}
+interface Prueba2 extends Prueba{    
+}
